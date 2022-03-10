792cf48d2eb609f5c12efe560d6ac656132d3fae
Danc-0 <lunjaludaniel@gmail.com>
2022-03-10 11:32:06 +0300

    project and file structure

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function Home() {
   return (
     <View style={styles.container}>
-      
+      <Text> Hello World this must end today and with all the Features if not then almost with everything.</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
---
+++ screens/index.js
@@ -0,0 +1,9 @@
+import Home from "./Home";
+import Splash from "./Splash";
+import SingleView from "./SingleView";
+
+export {
+    Splash,
+    Home,
+    SingleView
+}
